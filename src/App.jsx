8e4530603e3ed9128e32c1b9feaba5b53e559de8
Joshua Lemuel Z. Centina <128887473+whatataps123@users.noreply.github.com>
2024-08-21 05:52:57 +0000

    commit  web

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Joshua Lemuel",
+  title: "Software Engineer Intern",
+  email: "joshua.lemuel@dewise.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
